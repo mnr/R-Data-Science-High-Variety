1e9ab1718996924c3086b9e21cc7d4f7416cc96e
Mark Niemann-Ross <mark.niemannross@gmail.com>
2018-09-20 16:39:13 -0700

    first commit

---
+++ Chapter 01 (Excel)/01_02 Read Excel workbook.R
@@ -0,0 +1,61 @@
+# Copyright Mark Niemann-Ross, 2018
+# Author: Mark Niemann-Ross. mark.niemannross@gmail.com
+# LinkedIn: https://www.linkedin.com/in/markniemannross/
+# Github: https://github.com/mnr
+# More Learning: http://niemannross.com/link/mnratlil
+# Description: R Programming in Data Science: High Variety Data 
+
+# how to import an entire Excel workbook, including all sheets
+
+# be sure to set working directory
+# setwd("Exercise Files")
+
+# get a list of pathnames to files in Sample Files
+allSampleFiles <- list.files("Sample Files", full.names = TRUE)
+multipleSheetFile <- "Sample Files/multipleSheets.xlsx"
+
+
+# Rio for simplicity ------------------------------------------------------
+
+# install the Rio package - easy import
+install.packages("rio")
+library(rio)
+
+# Import one file with Rio
+import(allSampleFiles[1])
+
+# import multiple sheets as a list
+multisheetimport <- import_list(multipleSheetFile)
+# ...or... as a data.frame
+multisheetimport <- import_list(multipleSheetFile, rbind = TRUE)
+
+
+# import all files with Rio
+lotsaData <- import_list(allSampleFiles, rbind = TRUE)
+
+
+# or, use readxl ----------------------------------------------------------
+
+# readxl is previously installed by rio, but just in case
+# install.packages("readxl")
+# library(readxl)
+
+read_excel(allSampleFiles[1])
+
+# read all sheets in file
+excel_sheets(multipleSheetFile)
+
+lapply(excel_sheets(multipleSheetFile), read_excel, path = multipleSheetFile)
+
+# openxlsx only reads xlsx files ------------------------------------------
+
+# install.packages("openxlsx")
+# library(openxlsx)
+read.xlsx(allSampleFiles[1])
+
+# read all sheets in file
+lapply(getSheetNames(multipleSheetFile), read.xlsx, xlsxFile = multipleSheetFile)
+
+
+# oops...xls file...
+read.xlsx(list.files("Sample Files/", full.names = TRUE, pattern = "*.xls$"))
---
+++ Chapter 01 (Excel)/01_03 Write Excel workbook.R
@@ -0,0 +1,40 @@
+# Copyright Mark Niemann-Ross, 2018
+# Author: Mark Niemann-Ross. mark.niemannross@gmail.com
+# LinkedIn: https://www.linkedin.com/in/markniemannross/
+# Github: https://github.com/mnr
+# More Learning: http://niemannross.com/link/mnratlil
+# Description: R Programming in Data Science: High Variety Data 
+
+# how to create an entire Excel workbook, including multiple sheets
+
+# be sure to set working directory
+# setwd("Exercise Files")
+
+# two data.frames to be written to excel file
+data("CO2", "ChickWeight")
+
+# Rio for simplicity ------------------------------------------------------
+
+# install.packages("rio")
+# library(rio)
+
+export(list(CO2, ChickWeight ), file = "rioExport.xlsx", overwrite = TRUE)
+
+# or, use writexl ----------------------------------------------------------
+
+# writexl migh be previously installed by rio, but just in case
+# install.packages("writexl")
+# library(writexl)
+
+write_xlsx(list(CO2, ChickWeight ), path = "wxlsxExport.xlsx")
+
+
+# openxlsx only writes xlsx files, but lots of options ------------------------------------------
+
+# install.packages("openxlsx")
+# library(openxlsx)
+
+# note sheetname allows naming of sheets in workbook. Lots of other options - see documentation
+write.xlsx(list(CO2, ChickWeight ), 
+           file = "openxlExport.xlsx",
+           sheetName = c("CO2","ChickWeight"))
