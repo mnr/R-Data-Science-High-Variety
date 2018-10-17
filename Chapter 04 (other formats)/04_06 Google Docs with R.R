6342c319f0dbe6173fea9dc90b510acdfca139e4
Mark Niemann-Ross <mark.niemannross@gmail.com>
2018-10-17 15:42:56 -0700

    simplifying the lesson
    original lesson took 9 minutes. This should take 5

--- Chapter 04 (other formats)/04_06 Google Docs with R.R
+++ Chapter 04 (other formats)/04_06 Google Docs with R.R
@@ -8,24 +8,24 @@
 # CRAN has lots of google support packages.
 # This video only discusses import/export to google sheets
 
-install.packages("googlesheets")
-library(googlesheets)
-
-sheetsIOwn <- gs_ls() # this will initiate a browser auth session
-
-nameOfGoogleSheet <- "HighVarietyData_LinkedIn_Learning"
-
-# create and write a sheet to your google drive ----
-sampleGoogleSheet <- gs_new(nameOfGoogleSheet)
-
 # sample data
 simpleDF <- data.frame(This = c(1:5), That = c(letters[1:5]), another = c(LETTERS[1:5]))
 
+# install the googlesheets package if  you haven't already
+install.packages("googlesheets")
+library(googlesheets)
 
-gs_edit_cells(sampleGoogleSheet, input = simpleDF, anchor = "b3", verbose = TRUE)
+nameOfGoogleSheet <- "HighVarietyData_LinkedIn_Learning" # an existing google sheet
+
+# write data to an existing google sheet ----
+workingGS <- gs_title(nameOfGoogleSheet) # this will initiate a browser auth session
+
+gs_edit_cells(workingGS, input = simpleDF, anchor = "b3")
 
 # read that data back into R ----
-readingThisGoogleSheet <- gs_title(nameOfGoogleSheet) # can't read the gsheet until you identify it
+dataFromGS <- gs_read(workingGS)
 
-dataFromGS <- gs_read(readingThisGoogleSheet)
-
+# other useful GoogleSheet commands ----
+sheetsIOwn <- gs_ls() # list of sheets you have access to
+# create and write a sheet to your google drive ----
+sampleGoogleSheet <- gs_new(nameOfGoogleSheet)
